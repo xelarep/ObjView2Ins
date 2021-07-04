aebad59e283a88b33fd4431e326207505a4af6cf
xelarep <apeter@gmx.de>
2021-07-04 10:37:25 +0200

    Cleanup and more comments

--- objview2ins.py
+++ objview2ins.py
@@ -1,72 +1,67 @@
 # Read Export from ObjView generated tableExport.txt Multi Object List, and extract Multi Names
 #
+# ObjectView 0.98b beta by Brian Cowell, Mastering VAST forum --> https://forums.godlike.com.au
+# Rename .FSE to .FOR, open and ignore version errors, select MULTI and export as TXT 
+#
 # BBB, 03-JUL-2021
 
 from datetime import datetime
 
-insname = [] 						# create and empty list for multi Names
-bankcmpl = []
+fullINS = False										# build a complete file (True) or reduced (False)
 
-for idx in range (0, 1024+128):     # additional page for Multi 2048...
-	insname.append('-')				# marker for empty multi 
+patchnames = [] 									# create and empty list for multi Names
+bankactive = []
 
-
-fullINS = False							# build complete file (True) or reduced (False)
+# Create an empty list
+for idx in range (0, 1024+128):     				# additional page for Multi 2048...
+	patchnames.append('-')							# marker for empty multi 
 for idx in range(0,10):
-	bankcmpl.append(fullINS)
+	bankactive.append(fullINS)
 
-
-with open('tableExport.txt') as inputfile:
-	mylines = inputfile.readlines()
+with open('tableExport.txt') as inputfile:			# import ObjView output txt-file from ObjView 
+	linesread = inputfile.readlines()
 	inputfile.close()
 
-	for cur_line in mylines:
-		splitline = cur_line.split(',')
-		multi = splitline[1]					# multi number (1025...2048) 
-		name = splitline[2].replace('"','')		# read name and kick out "
+	for cur_line in linesread:
+		splittedline = cur_line.split(',')
+		multi = splittedline[1]						# multi number (1025...2048) as string
+		name = splittedline[2].replace('"','')		# get multi-name and kick out "
 		
-		if multi != 'ID#':
+		if multi != 'ID#':							# skip table header...
 			idx = int(multi)
-			insname[idx-1024] = name
+			patchnames[idx-1024] = name
 
 # Create ins.file
 if fullINS == True:
 	outfile = open ('fortese-full.ins','w') 
 else:
     outfile = open ('fortese.ins','w') 
+
 outfile.write(";Fortse SE Multi INS file\n")
 outfile.write(datetime.now().strftime("%d.%m.%Y - %H:%M:%S")+"\n\n")
 
 # Create Patch Names
 outfile.write(".Patch Names\n")
 
-midx = 1024
-midx2 = 1024
-for banks in range(0,9):
-	if fullINS == False:
-		for ridx in range(0,128):
-			if insname[midx2-1024] != '-':
-				bankcmpl[banks] = True
-			midx2 = midx2 + 1
+for patchbank in range(0,9):
+	if fullINS == False:									# check if selected bank got entries before writing the bank header...
+		for idx in range(0,128):
+			if patchnames[patchbank*128 + idx] != '-':
+				bankactive[patchbank] = True
 
-	if fullINS == True or bankcmpl[banks] == True:
-		outfile.write("\n[User " + str(banks+1) + " ("+str(midx)+"-"+str(midx+127)+")]\n")
-		for ridx in range(0,128):
-			if insname[midx-1024] != '-' or fullINS == True:
-				outfile.write(str(ridx)+"="+str(midx)+": "+insname[midx-1024]+"\n")
-			
-			midx = midx + 1
-	else:
-		midx = midx + 128
+	if bankactive[patchbank] == True:
+		outfile.write("\n[User " + str(patchbank+1) + " ("+str(patchbank*128  + 1024)+"-"+str(patchbank*128 +1024+127)+")]\n")
+		for idx in range(0,128):
+			if patchnames[patchbank*128 + idx] != '-' or fullINS == True:
+				outfile.write(str(idx)+"="+str(patchbank*128 + idx + 1024)+": "+patchnames[patchbank*128 + idx]+"\n")
+
 # Write instrument definition (we leave out the controllers)
 outfile.write("\n.Instrument Definitions\n")
 outfile.write("[Kurzweil Forte SE]\n")
 outfile.write("BankSelectMethod=2  ; Controller 32 only\n")
 
-midx = 1024
 for idx in range(0,9):
-	if fullINS == True or bankcmpl[idx] == True:
-		outfile.write("Patch["+f'{idx+8:03}'+"]=User " + str(idx+1) + " ("+str(midx)+"-"+str(midx+127)+")\n")
-	midx = midx + 128
+	if bankactive[idx] == True:
+		outfile.write("Patch["+f'{idx+8:03}'+"]=User " + str(idx+1) + " ("+str(idx*128+1024)+"-"+str(idx*128+1024+127)+")\n")
 
 outfile.close()
